a688937a3a3d7dc88d9da30785fefd24c7852672
Evanjohannes <wongely206@gmail.com>
2026-01-03 03:09:16 +0300

    Evanjohannes: Source code implementation

---
+++ src/Class1.java
@@ -0,0 +1,19 @@
+package Package1;
+
+
+/**
+ * @author user
+ * @version 1.0
+ * @created 23-Dec-2025 3:47:34 AM
+ */
+public class Class1 {
+
+	public Class1(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
